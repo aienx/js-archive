8e8ed11046a99098e45126d972178f149dc78d13
frederick ohene gh <66055564+isostack@users.noreply.github.com>
2022-11-29 06:57:22 -0800

    Upstream

---
+++ pages/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request , *args , **kwargs):
+    return render(request , "index.html" , {})
+def time(request , *args , **kwargs):
+    return render(request , "time.html" , {})
+def palind(request , *args , **kwargs):
+    return render(request , "palindrome.html" , {})
+def sch(request , *args , **kwargs):
+    return render(request , "schedule.html" , {})
+def stpw(request , *args , **kwargs):
+    return render(request , "stopw.html" , {})
+def tmpc(request , *args , **kwargs):
+    return render(request , "tempconv.html" , {})
+
